dfa0c1eba2de9130585190151d491b762740659a
Semight <davidoluwasemiloorei@gmail.com>
2023-05-04 23:04:14 +0100

    code modified

--- pages/categories/[category].tsx
+++ pages/categories/[category].tsx
@@ -6,59 +6,60 @@ import { useRouter } from "next/router";
 import { Alert } from "react-bootstrap";
 
 interface CategoryNewsPageProps {
-    newsArticles: NewsArticle[],
+  newsArticles: NewsArticle[];
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    const categorySlugs = [
-        "business",
-        "entertainment",
-        "general",
-        "health",
-        "science",
-        "sports",
-        "technology",
-    ];
+  const categorySlugs = [
+    "business",
+    "entertainment",
+    "general",
+    "health",
+    "science",
+    "sports",
+    "technology",
+  ];
 
-    const paths = categorySlugs.map(slug => ({ params: { category: slug } }));
+  const paths = categorySlugs.map((slug) => ({ params: { category: slug } }));
 
-    return {
-        paths,
-        fallback: false,
-    }
-}
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
-export const getStaticProps: GetStaticProps<CategoryNewsPageProps> = async ({ params }) => {
-    const category = params?.category?.toString();
-    const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`);
-    const newsResponse: NewsResponse = await response.json();
-    return {
-        props: { newsArticles: newsResponse?.articles },
-        revalidate: 5 * 60,
-    }
-    // let error go to 500 page
-}
+export const getStaticProps: GetStaticProps<CategoryNewsPageProps> = async ({
+  params,
+}) => {
+  const category = params?.category?.toString();
+  const response = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`
+  );
+  const newsResponse: NewsResponse = await response.json();
+  return {
+    props: { newsArticles: newsResponse?.articles },
+    revalidate: 5 * 60,
+  };
+  // let error go to 500 page
+};
 
 const CategoryNewsPage = ({ newsArticles }: CategoryNewsPageProps) => {
-    const router = useRouter();
-    const categoryName = router.query.category?.toString();
+  const router = useRouter();
+  const categoryName = router.query.category?.toString();
 
-    const title = "Category: " + categoryName;
+  const title = "Category: " + categoryName;
 
-    return (
-        <>
-            <Head>
-                <title key="title">{`${title} - NextJS News App`}</title>
-            </Head>
-            <main>
-                <h1>{title}</h1>
-                <Alert>
-          This Page uses <strong>getStaticProps</strong> for very highpage loading speed and <strong>Incremental static regeneration </strong> to show data not older than <strong>5 minutes.</strong>
-        </Alert>
-                <NewsArticlesGrid articles={newsArticles}/>
-            </main>
-        </>
-    );
-}
- 
-export default CategoryNewsPage ;
+  return (
+    <>
+      <Head>
+        <title key="title">{`${title} - NextJS News App`}</title>
+      </Head>
+      <main>
+        <h1>{title}</h1>
+        <NewsArticlesGrid articles={newsArticles} />
+      </main>
+    </>
+  );
+};
+
+export default CategoryNewsPage;
